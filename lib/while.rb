c4ecd26fa80f327c39edf5d1caa854b9d3ad5f83
Daniel Hope <upinhope@gmail.com>
2018-03-13 03:09:47 +0000

    automatically backed up by learn

--- lib/while.rb
+++ lib/while.rb
@@ -1,8 +1,8 @@
 def using_while
 	levitation_force = 6
 	
-	while levitation_force < 11
-	  puts "Wingarden Leviosa"
+	while levitation_force <= 10
+	  puts "Wingardiun Leviosa"
 	  levitation_force += 10
 	end
 end
